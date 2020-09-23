06a2e9ebf747cb63672dfd1c08330c7315b359d1
Frida Johansson <frida_mikaela@hotmail.com>
2020-09-23 21:30:55 +0200

    green tests for EventId

--- src/components/EventId.vue
+++ src/components/EventId.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="event">
     <div class="date">{{ date }}</div>
+    <div class="time">{{ time }}</div>
+    <div class="eventName">{{ eventName }}</div>
   </div>
 </template>
 
@@ -8,6 +10,8 @@
 export default {
   props: {
     date: String,
+    time: Number,
+    eventName: String,
   },
 };
 </script>
--- tests/unit/EventID.spec.js
+++ tests/unit/EventID.spec.js
@@ -10,3 +10,23 @@ it("should show the right date for the event in the events component", () => {
   let actual = wrapper.find(".date").text();
   expect(actual).toBe("FRI");
 });
+
+it("should show the right time for the event in the events component", () => {
+  const wrapper = shallowMount(EventId, {
+    propsData: {
+      time: 19,
+    },
+  });
+  let actual = wrapper.find(".time").text();
+  expect(actual).toBe("19");
+});
+
+it("should show the right event-name in the events component", () => {
+  const wrapper = shallowMount(EventId, {
+    propsData: {
+      eventName: "Frontend-developer meet",
+    },
+  });
+  let actual = wrapper.find(".eventName").text();
+  expect(actual).toBe("Frontend-developer meet");
+});
